fce5295d33891bc100e0f7a11d49fc81d2e69660
kelvindesaeger <kelvin.de.saeger@student.howest.be>
2026-01-09 16:05:53 +0100

    fixing build error

--- index.js
+++
@@ -1,2 +0,0 @@
-export { default as HandCursor } from "./src/HandCursor.jsx";
-export * from "./src/HandCursor.jsx"; // exports isPinching and clickAt
---
+++ src/index.js
@@ -0,0 +1,3 @@
+// src/index.js
+export { default as HandCursor } from "./HandCursor.jsx";
+export * from "./HandCursor.jsx"; // exports isPinching and clickAt
